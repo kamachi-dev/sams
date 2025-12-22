16d398f9b42819652ebb86d349924a736d4038e7
Machi <Angelo.kamachi03@gmail.com>
2025-12-22 18:20:48 +0800

    admin part 1

--- app/(pages)/admin-portal/page.tsx
+++ app/(pages)/admin-portal/page.tsx
@@ -1,32 +1,41 @@
 "use client";
 
 import SamsTemplate from "@/app/components/SamsTemplate";
-import { CopyIcon, ThickArrowRightIcon } from "@radix-ui/react-icons";
+import { CopyIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+import { Label } from "radix-ui";
 
 export default function Admin() {
     return (
         <SamsTemplate links={[
             {
-                label: "Dashboard",
-                Icon: ThickArrowRightIcon,
+                label: "SAMS+ Dataset",
+                Icon: () => <Image src="/icons/sheet.svg" alt="" width={20} height={20} />,
                 panels: [
-                    <div key={1}>WAHA</div>,
-                    <div key={2}>I love Lasagna</div>,
-                    <div key={2}>Panel 2 <CopyIcon /> TEST TEST</div>
+                    <div key={1} className="flex justify-center items-center gap-4 h-full">
+                        <Image src="/icons/people.svg" alt="" width={40} height={40} />
+                        <div className="flex flex-col items-center">
+                            <Label.Root className="font-bold">Total Num of Students</Label.Root>
+                            <span>1500</span>
+                        </div>
+                    </div>,
+                    <div key={2} className="flex justify-center items-center gap-4 h-full">
+                        <Image src="/icons/people.svg" alt="" width={40} height={40} />
+                        <div className="flex flex-col items-center">
+                            <Label.Root className="font-bold">Total Num of Teachers</Label.Root>
+                            <span>1500</span>
+                        </div>
+                    </div>,
+                    <div key={3} className="flex justify-center items-center gap-4 h-full">
+                        <Image src="/icons/notebook.svg" alt="" width={40} height={40} />
+                        <div className="flex flex-col items-center">
+                            <Label.Root className="font-bold">Total Num of Classes</Label.Root>
+                            <span>1500</span>
+                        </div>
+                    </div>
                 ],
                 content: <>
                     Dashboard shenanigans
-                </>
-            },
-            {
-                label: "Notifications",
-                Icon: ThickArrowRightIcon,
-                panels: [
-                    <div key={1}>Panel 1</div>,
-                    <div key={2}>Panel 2</div>
-                ],
-                content: <>
-                    You got mail
                 </>
             }
         ]} />
